533eb443c306c0f6a83c2b30f6bf48b06159fc62
Karthik Kovi <karthi.kovi@gmail.com>
2020-05-18 23:38:45 +0930

    Used await async instead of promises

--- app.js
+++ app.js
@@ -11,129 +11,108 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./lib/htmlRenderer");
 
 const employees = [];
+let newEntry = false;
 
-const manager = function() {
-    return new Promise ((res, rej) => {
-        inquirer.prompt([
-            {
-                message: "Enter manager name - ",
-                name: "name"
-            },
-            {
-                message: "Enter manager ID - ",
-                name: "id"
-            },
-            {
-                message: "Enter email ID - ",
-                name: "email"
-            },
-            {
-                message: "Enter Office number - ",
-                name: "officeNumber"
-            }
-        ]).then((data) => {
-            employees.push(new Manager(data.name, data.id, data.email, data.officeNumber));
-        } )
-    })
+function getManager() {
+    return inquirer.prompt([
+        {
+            name: "name",
+            message: "What is the manager's name"
+        },
+        {
+            name: "id",
+            message: "What is the manager's ID"
+        },
+        {
+            name: "email",
+            message: "Enter the manager email"
+        },
+        {
+            name: "officeNumber",
+            message: "Enter the office number"
+        }
+    ])
 }
 
-
-const employee = function() {
-    return new Promise ((res, rej) => {
-        inquirer.prompt([
-            {
-                message: "Do you wish to add an employee or intern",
-                type: "list",
-                name: "type",
-                choices: [
-                    "Engineer",
-                    "Intern",
-                    "I am done adding employees"
-                ]
-            }
-        ]).then(data => {
-            switch (data.type) {
-                case "Engineer":
-                    return new Promise ((res, rej) => {
-                        inquirer.prompt([
-                            {
-                                message: "What is the Engineer's name - ",
-                                name: "name"
-                            },
-                            {
-                                message: "What is the ID - ",
-                                name: "id"
-                            },
-                            {
-                                message: "What is their email - ",
-                                name: "email"
-                            },
-                            {
-                                message: "What is their GitHub username - ",
-                                name: "github"
-                            }
-                        ])
-                    }).then((data)=> {
-                        employees.push(new Engineer(data.name, data.id, data.email, data.github))
-                    })
-                break;
-                case "Intern":
-                    return new Promise ((res, rej) => {
-                        inquirer.prompt([
-                            {
-                                message: "What is the Engineer's name - ",
-                                name: "name"
-                            },
-                            {
-                                message: "What is the ID - ",
-                                name: "id"
-                            },
-                            {
-                                message: "What is their email - ",
-                                name: "email"
-                            },
-                            {
-                                message: "What is their School - ",
-                                name: "school"
-                            }
-                        ])
-                    }).then((data)=> {
-                        employees.push(new Intern(data.name, data.id, data.email, data.school))
-                    })
-                break;
-
-                case  "I am done adding employees":
-                    break;
-            }
-        })
-    })
+function employeeTypefn() {
+    return inquirer.prompt([
+        {
+            type: "list",
+            name: "employeeType",
+            message: "Do you want to add an intern or an engineer",
+            choices: ["Intern", "Engineer", "Done"]
+        }
+    ])
 }
 
-let html = render(employees)
+function engineerfn() {
+    return inquirer.prompt([
+        {
+            name: "name",
+            message: "Enter the name of the Engineer"
+        },
+        {
+            name: "id",
+            message: "Enter the engineer ID"
+        },
+        {
+            name: "email",
+            message: "Enter the email ID"
+        },
+        {
+            name: "gitHub",
+            message: "Enter the gitHub username"
+        }
+    ])
+}
 
-fs.writeFile(outputPath, html, (err) => {
-    if (err) throw err;
-    console.log(`HTML file created in ${outputPath}`)
-})
+function internfn() {
+    return inquirer.prompt([
+        {
+            name: "name",
+            message: "Enter the name of the Intern"
+        },
+        {
+            name: "id",
+            message: "Enter the intern ID"
+        },
+        {
+            name: "email",
+            message: "Enter the email ID"
+        },
+        {
+            name: "school",
+            message: "Enter the School name"
+        }
+    ])
+}
 
-function init() {
-    return new Promise((res, rej) => {
-        inquirer.prompt([{
-            message: "Do you wish to add a manager or an employee",
-                type: "list",
-                name: "type",
-                choices: [
-                    "Manager",
-                    "Employee",
-                ]
-        }]).then((data => {
-            if(data.type === "Manager") {
-                manager()
-            } else {
-                employee()
-            }
-        }))
-    })
+async function init() {
+
+    const manager = await getManager();
+    employees.push(new Manager(manager.name, manager.id, manager.email, manager.officeNumber))
+    while (!newEntry) {
+
+        let employeeType = await employeeTypefn();
+
+        if (employeeType.employeeType === "Engineer") {
+            let engineer = await engineerfn();
+            employees.push(new Engineer(engineer.name, engineer.id, engineer.email, engineer.gitHub))
+        } else if (employeeType.employeeType === "Intern") {
+            let intern = await internfn();
+            employees.push(new Intern(intern.name, intern.id, intern.email, intern.school))
+        } else {
+            let html = render(employees)
+
+            fs.writeFile(outputPath, html, (err) => {
+                if (err) {
+                    throw err
+                }
+                console.log(`File is written at ${outputPath}`)
+                newEntry = true;
+            })
+        }
+    }
 }
 
 init()
